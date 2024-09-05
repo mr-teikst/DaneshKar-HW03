212202e43224049a89a713d6717bed71d80fd321
erfan._.teikst <erfan.teist2027@gmail.com>
2024-09-05 12:44:18 +0330

    ex 05 completed

--- 05-findMax/findMax.js
+++ 05-findMax/findMax.js
@@ -1,1 +1,11 @@
-// write your func here
+const maxFinder = (arr) => {
+  let max = arr[0];
+  arr.forEach((element) => {
+    if (element > max) {
+      max = element;
+    }
+  });
+  return max;
+};
+
+// console.log(maxFinder([12, 1, 8, 20, 5]));
