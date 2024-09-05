916e626838c7d25563cc081786ef4a8cd3d022c0
erfan._.teikst <erfan.teist2027@gmail.com>
2024-09-05 12:37:50 +0330

    ex 03 completed

--- 03-factoriel/fac.js
+++ 03-factoriel/fac.js
@@ -1,1 +1,9 @@
-// write your func here
+const factorial = (num) => {
+  let result = 1;
+  for (let i = 1; i < num + 1; i++) {
+    result *= i;
+  }
+  return result;
+};
+
+// console.log(factorial(12));
