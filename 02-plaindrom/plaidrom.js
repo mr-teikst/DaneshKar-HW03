19436064f6f105bd5a615113ec8387664136d906
erfan._.teikst <erfan.teist2027@gmail.com>
2024-09-05 12:35:31 +0330

    ex 02 completed

--- 02-plaindrom/plaidrom.js
+++ 02-plaindrom/plaidrom.js
@@ -1,1 +1,11 @@
-// write your func here
+const palindrome = (str) => {
+  let result = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    result += str[i];
+  }
+  if (str === result) return true;
+  else return false;
+};
+
+// console.log(palindrome("racecar"));
+// console.log(palindrome("dsdslfdf"));
