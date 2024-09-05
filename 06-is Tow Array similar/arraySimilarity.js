23d7243d651daef8834d9b26f9baa88bd2420a37
erfan._.teikst <erfan.teist2027@gmail.com>
2024-09-05 12:46:07 +0330

    ex 06 completed

--- 06-is Tow Array similar/arraySimilarity.js
+++ 06-is Tow Array similar/arraySimilarity.js
@@ -1,1 +1,10 @@
-// write your func here
+const isArrayEqual = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+};
+
+// console.log(isArrayEqual([1, 2, 3], [1, 2, 3]));
+// console.log(isArrayEqual([1, 2, 3], [8, 9, 10]));
